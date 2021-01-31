96edb96e1e2bd4714dad0f1fd61b92b2955dfafa
Thezerix <gallowsdove@github.com>
2021-01-31 01:02:23 +0100

    Added SlimefunItemStack support for GalacticPopulator

--- src/main/java/io/github/addoncommunity/galactifun/api/GalacticPopulator.java
+++ src/main/java/io/github/addoncommunity/galactifun/api/GalacticPopulator.java
@@ -8,12 +8,17 @@ package io.github.addoncommunity.galactifun.api;
 *
  */
 
+import io.github.addoncommunity.galactifun.Galactifun;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,9 +33,23 @@ public class GalacticPopulator extends BlockPopulator {
     private final int maxy;
     private final int minSize;
     private final int maxSize;
-    @Nonnull Material ore;
-    @Nonnull List<Material> source;
+    @Nonnull private final Material ore;
+    @Nullable private final String id;
+    @Nonnull private final List<Material> source;
 
+
+    public GalacticPopulator(int attempts, int chance, int miny, int maxy, int minSize, int maxSize, @Nonnull SlimefunItemStack slimefunItem,
+                             @Nonnull Material... source) {
+        this.attempts = attempts;
+        this.chance = chance;
+        this.miny = miny;
+        this.maxy = maxy;
+        this.minSize = minSize;
+        this.maxSize= maxSize;
+        this.ore = slimefunItem.getType();
+        this.id = slimefunItem.getItemId();
+        this.source = new ArrayList<>(Arrays.asList(source));
+    }
 
     public GalacticPopulator(int attempts, int chance, int miny, int maxy, int minSize, int maxSize, @Nonnull Material ore,
                              @Nonnull Material... source) {
@@ -41,9 +60,8 @@ public class GalacticPopulator extends BlockPopulator {
         this.minSize = minSize;
         this.maxSize= maxSize;
         this.ore = ore;
+        this.id = null;
         this.source = new ArrayList<>(Arrays.asList(source));
-
-
     }
 
     @Override
@@ -60,6 +78,16 @@ public class GalacticPopulator extends BlockPopulator {
 
                     while (canContinue) {
                         chunk.getBlock(x, y, z).setType(ore);
+                        if (id != null) {
+                            final int fx = x;
+                            final int fy = y;
+                            final int fz = z;
+
+                            // Cam produce concurrentModificationException error, currently non-avoidable
+                            Bukkit.getScheduler().runTask(Galactifun.getInstance(),
+                                    () -> BlockStorage.store(chunk.getBlock(fx, fy, fz), id));
+                        }
+
                         if ((length < minSize) || (random.nextInt(100) < 50)) {
                             switch (random.nextInt(6)) {
                                 case 0:
