40b1bdcae9a1a060d9a29e8ee522c1eaa662e14b
Thezerix <gallowsdove@github.com>
2021-02-02 23:52:15 +0100

    Added creative check

--- src/main/java/io/github/addoncommunity/galactifun/base/aliens/AlienCreeper.java
+++ src/main/java/io/github/addoncommunity/galactifun/base/aliens/AlienCreeper.java
@@ -3,6 +3,7 @@ package io.github.addoncommunity.galactifun.base.aliens;
 import io.github.addoncommunity.galactifun.api.mob.Alien;
 import io.github.addoncommunity.galactifun.api.universe.CelestialWorld;
 import org.bukkit.Chunk;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -47,12 +48,18 @@ public class AlienCreeper extends Alien {
     protected void onHit(@Nonnull EntityDamageByEntityEvent e) {
         Creeper creeper = (Creeper) e.getEntity();
         if (e.getDamager() instanceof Player) {
-            creeper.setTarget((Player) e.getDamager());
+            Player p = (Player) e.getDamager();
+            if (p.getGameMode() != GameMode.CREATIVE) {
+                creeper.setTarget((Player) e.getDamager());
+            }
         }
         else if (e.getDamager() instanceof Projectile) {
-            Projectile p = (Projectile) e.getDamager();
-            if (p.getShooter() instanceof Player) {
-                creeper.setTarget((Player) p.getShooter());
+            Projectile pr = (Projectile) e.getDamager();
+            if (pr.getShooter() instanceof Player) {
+                Player p = (Player) pr.getShooter();
+                if (p.getGameMode() != GameMode.CREATIVE) {
+                    creeper.setTarget(p);
+                }
             }
         }
     }
