f5dfee85856a05ec618442119b701ccd99ae8fc7
Seggan <segganew@gmail.com>
2021-09-18 17:52:15 -0400

    Fix missing life support module head

--- src/main/java/io/github/addoncommunity/galactifun/base/GalactifunHead.java
+++ src/main/java/io/github/addoncommunity/galactifun/base/GalactifunHead.java
@@ -14,7 +14,7 @@ public final class GalactifunHead {
     public static final ItemStack OXYGEN_REGENERATOR = SlimefunUtils.getCustomHead("8be7bc66389d96a9c5f4aa5a87579ee661e88ada6f7b8be174bf3982a34f84");
     public static final ItemStack FAN = SlimefunUtils.getCustomHead("172771fc11737063c81eb75973e9b82590951e3674f31acce8cddd1aad1b");
     public static final ItemStack BOX = SlimefunUtils.getCustomHead("b2a1d4f4b563827a441f7734e4df3c3b89fc229268c863f1dceedcadfde6423a");
-    public static final ItemStack LIFE_SUPPORT_MODULE = SlimefunUtils.getCustomHead("3625be5ba96fc11448aaddb915a390391c3f92f4af81a4a2adb0e3f4f65");
+    public static final ItemStack LIFE_SUPPORT_MODULE = SlimefunUtils.getCustomHead("5aa7c7bccf903cec5f3d0323ad05054aa207d5412f5097d0ab214cf59b42bc1c");
     public static final ItemStack CAN = SlimefunUtils.getCustomHead("eb2e58962331676f67a67c0273ed29ba4465986b44e1598e0b8a29c04f82d21a");
 
 }
