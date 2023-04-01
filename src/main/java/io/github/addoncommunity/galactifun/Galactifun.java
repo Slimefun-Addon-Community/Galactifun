f9719207a2ad8ea0171dd4b968b03362aa175867
Seggan <segganew@gmail.com>
2023-04-01 10:05:47 -0400

    Nuke pretty much everything

--- src/main/java/io/github/addoncommunity/galactifun/Galactifun.java
+++ src/main/java/io/github/addoncommunity/galactifun/Galactifun.java
@@ -3,32 +3,10 @@ package io.github.addoncommunity.galactifun;
 import java.io.File;
 import java.util.logging.Level;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import lombok.Getter;
-
 import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
-import io.github.addoncommunity.galactifun.api.worlds.AlienWorld;
-import io.github.addoncommunity.galactifun.api.worlds.PlanetaryWorld;
-import io.github.addoncommunity.galactifun.base.BaseAlien;
-import io.github.addoncommunity.galactifun.base.BaseItems;
-import io.github.addoncommunity.galactifun.base.BaseMats;
-import io.github.addoncommunity.galactifun.base.BaseUniverse;
-import io.github.addoncommunity.galactifun.core.CoreItemGroup;
-import io.github.addoncommunity.galactifun.core.commands.AlienSpawnCommand;
-import io.github.addoncommunity.galactifun.core.commands.EffectsCommand;
-import io.github.addoncommunity.galactifun.core.commands.GalactiportCommand;
-import io.github.addoncommunity.galactifun.core.commands.SealedCommand;
-import io.github.addoncommunity.galactifun.core.commands.StructureCommand;
-import io.github.addoncommunity.galactifun.core.managers.AlienManager;
-import io.github.addoncommunity.galactifun.core.managers.ProtectionManager;
-import io.github.addoncommunity.galactifun.core.managers.WorldManager;
 import io.github.mooy1.infinitylib.common.Scheduler;
 import io.github.mooy1.infinitylib.core.AbstractAddon;
 import io.github.mooy1.infinitylib.metrics.bukkit.Metrics;
@@ -39,14 +17,7 @@ import io.github.thebusybiscuit.slimefun4.libraries.paperlib.PaperLib;
 
 public final class Galactifun extends AbstractAddon {
 
-    @Getter
     private static Galactifun instance;
-
-    private boolean isTest = false;
-
-    private AlienManager alienManager;
-    private WorldManager worldManager;
-    private ProtectionManager protectionManager;
 
     private boolean shouldDisable = false;
 
@@ -56,61 +27,34 @@ public final class Galactifun extends AbstractAddon {
 
     public Galactifun(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
         super(loader, description, dataFolder, file, "Slimefun-Addon-Community", "Galactifun", "master", "auto-update");
-        isTest = true;
-    }
-
-    public static AlienManager alienManager() {
-        return instance.alienManager;
-    }
-
-    public static WorldManager worldManager() {
-        return instance.worldManager;
-    }
-
-    public static ProtectionManager protectionManager() {
-        return instance.protectionManager;
     }
 
     @Override
     protected void enable() {
         instance = this;
 
-        if (!isTest) {
-            if (!PaperLib.isPaper()) {
-                log(Level.SEVERE, "Galactifun only supports Paper and its forks (i.e. Airplane and Purpur)");
-                log(Level.SEVERE, "Please use Paper or a fork of Paper");
-                shouldDisable = true;
-            }
-            if (Slimefun.getMinecraftVersion().isBefore(MinecraftVersion.MINECRAFT_1_17)) {
-                log(Level.SEVERE, "Galactifun only supports Minecraft 1.17 and above");
-                log(Level.SEVERE, "Please use Minecraft 1.17 or above");
-                shouldDisable = true;
-            }
-            if (Bukkit.getPluginManager().isPluginEnabled("ClayTech")) {
-                log(Level.SEVERE, "Galactifun will not work properly with ClayTech");
-                log(Level.SEVERE, "Please disable ClayTech");
-                shouldDisable = true;
-            }
+        if (!PaperLib.isPaper()) {
+            log(Level.SEVERE, "Galactifun only supports Paper and its forks (i.e. Airplane and Purpur)");
+            log(Level.SEVERE, "Please use Paper or a fork of Paper");
+            shouldDisable = true;
+        }
+        if (Slimefun.getMinecraftVersion().isBefore(MinecraftVersion.MINECRAFT_1_17)) {
+            log(Level.SEVERE, "Galactifun only supports Minecraft 1.17 and above");
+            log(Level.SEVERE, "Please use Minecraft 1.17 or above");
+            shouldDisable = true;
+        }
+        if (Bukkit.getPluginManager().isPluginEnabled("ClayTech")) {
+            log(Level.SEVERE, "Galactifun will not work properly with ClayTech");
+            log(Level.SEVERE, "Please disable ClayTech");
+            shouldDisable = true;
+        }
 
-            if (shouldDisable) {
-                Bukkit.getPluginManager().disablePlugin(this);
-                return;
-            }
+        if (shouldDisable) {
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
         }
 
         new Metrics(this, 11613);
-
-        this.alienManager = new AlienManager(this);
-        this.worldManager = new WorldManager(this);
-        this.protectionManager = new ProtectionManager();
-
-        BaseAlien.setup(this.alienManager);
-        if (!isTest) {
-            BaseUniverse.setup(this);
-        }
-        CoreItemGroup.setup(this);
-        BaseMats.setup();
-        BaseItems.setup(this);
 
         // log after startup
         Scheduler.run(() -> log(Level.INFO,
@@ -122,20 +66,11 @@ public final class Galactifun extends AbstractAddon {
                 "",
                 "###################################################"
         ));
-
-        getAddonCommand()
-                .addSub(new GalactiportCommand())
-                .addSub(new AlienSpawnCommand())
-                .addSub(new StructureCommand(this))
-                .addSub(new SealedCommand())
-                .addSub(new EffectsCommand());
     }
 
     @Override
     protected void disable() {
         if (shouldDisable) return;
-
-        this.alienManager.onDisable();
 
         // Do this last
         instance = null;
@@ -143,24 +78,7 @@ public final class Galactifun extends AbstractAddon {
 
     @Override
     public void load() {
-        if (!isTest) {
-            // Default to not logging world settings
-            Bukkit.spigot().getConfig().set("world-settings.default.verbose", false);
-        }
+        // Default to not logging world settings
+        Bukkit.spigot().getConfig().set("world-settings.default.verbose", false);
     }
-
-    @Nullable
-    @Override
-    public ChunkGenerator getDefaultWorldGenerator(@Nonnull String worldName, @Nullable String id) {
-        World world = Bukkit.getWorld(worldName);
-        if (world == null) return null;
-
-        PlanetaryWorld planetaryWorld = this.worldManager.getWorld(world);
-        if (planetaryWorld instanceof AlienWorld) {
-            return planetaryWorld.world().getGenerator();
-        }
-
-        return null;
-    }
-
 }
